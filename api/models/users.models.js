a1aaec201e71f361049d1f9663c20a01138851bd
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-06 19:31:22 +0100

    Changes in this files

--- api/models/users.models.js
+++ api/models/users.models.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
+
 const usersSchema = new Schema({
   name: String,
   surnames: String,
--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post("/users", async (req, res) => {
   try {
 	await User.create(req.body)
+
     res.status(200).json({
       status: 200,
       data: req.body,
@@ -16,7 +17,7 @@ router.post("/users", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users", (req, res) => {
   try {
 	  User.find({}, (err,found) =>{
 		  if(found === null){
