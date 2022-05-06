fbe8aa1c17dbfdea656459dc423cbac17288b7ed
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-06 10:21:32 +0100

    🔧 BUILD: Added route post for users

---
+++ api/routes/users.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const User = require("../models/users.models");
+const router = express.Router();
+
+router.post("/users", async (req, res) => {
+  try {
+    let user = new User(req.body);
+    user = await user.save();
+    res.status(200).json({
+      status: 200,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+});
+module.exports = router;
