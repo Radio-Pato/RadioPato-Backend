d5639d4fe207ac646f92122ba810b45f835ab632
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-07 15:49:12 +0100

    ➕ Added routes for login and register

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,36 +1,15 @@
 const express = require("express");
-const User = require("../models/users.models");
+const {getByEmail,getValidLogin, create}= require("../controller/users.controller");
 const router = express.Router();
-router.post("/users", async (req, res) => {
-  try {
-	await User.create(req.body)
 
-    res.status(200).json({
-      status: 200,
-      data: req.body,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 400,
-      message: error.message,
-    });
-  }
-});
+//Solo test
+router.get("/:email",getByEmail)
 
-router.get("/users", (req, res) => {
-  try {
-	  User.find({}, (err,found) =>{
-		  if(found === null){
-			res.status(200).json("Usuario no encontrado")
-		  }
-		 res.status(200).json(found)
-	})
+//Acceso del usuario
+router.get("/acceso",getValidLogin)
 
-  } catch (error) {
-    res.status(400).json({
-      status: 400,
-      message: error.message,
-    });
-  }
-});
+//Registro del usuario
+router.post("/registro", create);
+
+
 module.exports = router;
