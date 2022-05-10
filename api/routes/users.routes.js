289c02c25dd1c3af9c0dc998e866550fcc7ed7f7
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-10 17:05:34 +0100

    🎉 routes

---
+++ api/routes/users.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+//POST
+//Get email and password for login
+//router.post("/access/")
+//Register new user
+//router.post("/register/")
+//get comments
+//router.get("/comments/")
+
+
+//PUT
+//Edit user
+//router.put("/edit/:email")
+
+//DEL
+//Delete user
+//router.delete("/:email")
