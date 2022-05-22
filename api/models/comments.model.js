b9642c1377db42b0d063ce7ba949d21364199732
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-05-22 16:29:24 +0100

    🐛 solo modifique un comentario

--- api/models/comments.model.js
+++ api/models/comments.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-//Section data schema
+//Comments data schema
 const commentModel = new Schema({
   owner: {
     type: String,
